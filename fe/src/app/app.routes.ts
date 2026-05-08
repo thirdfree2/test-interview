fe2418b68d5dcf8e9b48376a412801e2dab4bf16
Third Pativeth <126750481+thirdfree2@users.noreply.github.com>
2026-05-08 17:48:18 +0700

    [FE] Init Angular Tailwind, App Route

--- fe/src/app/app.routes.ts
+++ fe/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from '../pages/login/login.component';
+import { MyProfileComponent } from '../pages/my-profile/my-profile.component';
+import { RegisterComponent } from '../pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LoginComponent,
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
+    },
+    {
+        path: 'my-profile',
+        component: MyProfileComponent,
+    },
+];
